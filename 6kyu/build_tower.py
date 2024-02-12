0329e6200bbabe66f7aeb98d34361acfeb8b669c
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-12 01:30:27 -0800

    Add shorter_version of build_tower

--- 6kyu/build_tower.py
+++ 6kyu/build_tower.py
@@ -21,14 +21,18 @@ And a tower with 6 floors looks like this:
 ]
 '''
 
+# def tower_builder(n_floors):
+#   odd_nums = [n for n in range(n_floors * 2) if n % 2]
+#   tower = []
+
+#   for num in odd_nums:
+#     tower.append(("*" * num).center(odd_nums))
+
+#   return tower
+
 def tower_builder(n_floors):
   odd_nums = [n for n in range(n_floors * 2) if n % 2]
-  tower = []
-
-  for num in odd_nums:
-    tower.append(("*" * num).center(odd_nums[-1]))
-
-  return tower
+  return [("*" * num).center(odd_nums[-1]) for num in odd_nums]
 
 print(tower_builder(3))
 print(tower_builder(5))
