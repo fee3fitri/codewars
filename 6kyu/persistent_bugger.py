45596e8617be5b98f374faf28de7f5e77bbee4ee
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-12 22:50:13 -0800

    Add iterative solution to persistent_bugger

--- 6kyu/persistent_bugger.py
+++ 6kyu/persistent_bugger.py
@@ -1,14 +1,29 @@
+# def persistence(n):
+#   if n < 10:
+#     return 0
+  
+#   total = 1
+#   for i in str(n):
+#     total *= int(i)
+  
+#   # Add one as a counter for each iteration
+#   # If only call the function, it will return 0 since the last call will always satisfies the base casae
+#   return persistence(total) + 1 
+
+
+# Using iteration
 def persistence(n):
-  if n < 10:
-    return 0
+  count = 0
   
-  total = 1
-  for i in str(n):
-    total *= int(i)
-  
-  # Add one as a counter for each iteration
-  # If only call the function, it will return 0 since the last call will always satisfies the base casae
-  return persistence(total) + 1 
+  while n > 9:
+    count += 1
+
+    total = 1
+    for i in str(n):
+      total *= int(i)
+    n = total
+
+  return count
   
   
 print(persistence(39))
