63604b96ec5f403cab508a8818578c179f361197
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-14 02:08:30 -0800

    refactor convoluted solution for to_camel_case problem

--- 6kyu/convert_string_to_camel_case.py
+++ 6kyu/convert_string_to_camel_case.py
@@ -11,16 +11,22 @@ Examples
 
 import re
 
+# def to_camel_case(text):
+#   words = re.sub('[^a-zA-Z]+', ' ', text).split()
+#   res = []
+#   for i in range(len(words)):
+#     if i == 0:
+#       res.append(words[i])
+#     else:
+#       res.append(words[i].capitalize())
+#   # return ''.join(words)
+#   return ''.join(res)
+
 def to_camel_case(text):
-  words = re.sub('[^a-zA-Z]+', ' ', text).split()
-  res = []
-  for i in range(len(words)):
-    if i == 0:
-      res.append(words[i])
-    else:
-      res.append(words[i].capitalize())
-  # return ''.join(words)
-  return ''.join(res)
+  words = text.replace('-', ' ').replace('_', ' ').split()
+  if len(words) == 0:
+    return ''
+  return words[0] + ''.join(word.capitalize() for word in words[1:])
 
 print(to_camel_case("the-stealth-warrior"))
 print(to_camel_case("The_Stealth_Warrior"))
