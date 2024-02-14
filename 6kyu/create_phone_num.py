441894c76a0674ea4df18d3f02b7b5217394def4
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-14 12:34:26 -0800

    Add create_phone_num using map

--- 6kyu/create_phone_num.py
+++ 6kyu/create_phone_num.py
@@ -9,5 +9,11 @@ Don't forget the space after the closing parentheses!
 '''
 
 def create_phone_number(n):
-  m = ''.join([str(num) for num in n])
+  m = ''.join(map(str, n))
   return f"({m[:3]}) {m[3:6]}-{m[6:]}"
+
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]))
+print(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
