8ff78f6ca1803277664119bf7580e2cab15b2647
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-19 23:02:54 -0800

    Add unique_in_order

---
+++ 6kyu/unique_in_order.py
@@ -0,0 +1,26 @@
+'''
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
+unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]
+'''
+
+def unique_in_order(sequence):
+  if len(sequence) == 0:
+    return []
+  
+  listed = list(sequence)
+  res = [listed[0]]
+
+  for i in range(1, len(listed)):
+    if listed[i] != listed[i-1]:
+      res.append(listed[i])
+  
+  return res
+
+print(unique_in_order([1, 2, 3, 3, -1]))
+print(unique_in_order('AAAABBBCCDAABBB'))
